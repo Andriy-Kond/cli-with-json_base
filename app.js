043139b3ab0c3a6f04693204a3df7529ee9ffd91
Andrii <andrii.kondratovich@gmail.com>
2024-11-27 16:39:59 +0200

    add: API db

--- app.js
+++ app.js
@@ -5,22 +5,44 @@
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
-import { getAll } from "./db/index.js";
-console.log("getAll:::", getAll);
+import * as db from "./db/index.js";
 
 const contactsProcessing = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "read":
-      const contacts = await getAll();
-      return console.log("contactsProcessing >> contacts:::", contacts);
+    case "readContact":
+      const contacts = await db.getAllContacts();
+      return console.log("contacts:::", contacts);
 
-    // case "getById":
-    // case "deleteById":
+    case "getContactById":
+      const contactById = await db.getContactById(id);
+      return console.log("contactById:::", contactById);
+
+    case "addContact":
+      const newContact = await db.addContact({ name, email, phone });
+      return console.log("newContact:::", newContact);
+
+    case "editContact":
+      const editedContact = await db.editContact({ id, name, email, phone });
+      return console.log("editedContact:::", editedContact);
+
     default:
       return console.log("Unknown action");
   }
 };
 
-contactsProcessing({ action: "read" });
-// contactsProcessing({ action: "getById", id: "qdggE76Jtbfd9eWJHrssH" });
-// contactsProcessing({ action: "deleteById" });
+// contactsProcessing({ action: "readContact" });
+// contactsProcessing({ action: "getContactById", id: "qdggE76Jtbfd9eWJHrssH" });
+// contactsProcessing({
+//   action: "addContact",
+//   name: "Andrii",
+//   email: "andrii@mail.com",
+//   phone: "555 555 5555",
+// });
+
+contactsProcessing({
+  action: "editContact",
+  id: "2mxsoFRHK5atAIMRGPP1N",
+  name: "Andrii-2",
+  email: "andrii@mail.com",
+  phone: "555 555 5555",
+});
--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
-// ! Functions for work with db (contacts.json)
+// ! Functions for work with db (aka contacts.json)
 
+import { nanoid } from "nanoid";
 // У Node.js починаючи з v18.0.0 для імпорту JSON через import потрібно вказувати атрибут assert з типом json.
 import contacts from "./contacts.json" assert { type: "json" };
 // const contacts = require("./contacts");
@@ -8,14 +9,47 @@ import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // path to file in ESM
+const __dirname = path.dirname(__filename); // path to dir in ESM
+const contactsPath = path.join(__dirname, "contacts.json"); // Normalize slashes
 
-const getAll = async () => {
-  const contactsBookBuffer = await fs.readFile(
-    path.join(__dirname, "contacts.json"),
-  );
-  return JSON.parse(contactsBookBuffer);
+export const getAllContacts = async () => {
+  const contactsBookBuffer = await fs.readFile(contactsPath);
+  return JSON.parse(contactsBookBuffer); // make obj from json
 };
 
-export { getAll };
+export const getContactById = async id => {
+  const contacts = await getAllContacts();
+  const contact = contacts.find(contact => contact.id === id); // find contact by id
+  return contact || null; // real database behavior
+};
+
+export const addContact = async data => {
+  const contacts = await getAllContacts();
+  const newContact = { id: nanoid(), ...data };
+  contacts.push(newContact); // add to contacts array
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)); // 2 - number off spaces. Making .json-file with normal formatting (otherwise all will bi in one line)
+
+  return newContact;
+};
+
+export const editContact = async data => {
+  console.log("editContact >> data:::", data);
+  const contacts = await getAllContacts();
+  const index = contacts.findIndex(contact => contact.id === data.id);
+  console.log("editContact >> index:::", index);
+  if (index === -1) {
+    console.log("This id not in our db");
+    return null;
+  }
+  contacts[index] = data;
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
+
+// fs.readFile(filename, [options]) – читання файлу
+// fs.writeFile(filename, data, [options]) – запис файлу
+// fs.appendFile(filename, data, [options]) – додавання у файл
+// fs.rename(oldPath, newPath) – перейменування файлу.
+// fs.unlink(path, callback) – видалення файлу.
